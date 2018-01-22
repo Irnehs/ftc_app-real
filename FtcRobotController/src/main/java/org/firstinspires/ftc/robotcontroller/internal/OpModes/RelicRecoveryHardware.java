fccd372f0fcb75c63fd2cfbe9ac6e9c02a5e4022
kevinr03 <kevin.n.rockwell@gmail.com>
2018-01-22 15:37:19 -0800

    Testing any issues

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/OpModes/RelicRecoveryHardware.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/OpModes/RelicRecoveryHardware.java
@@ -92,9 +92,6 @@ public class RelicRecoveryHardware
         //gyroSensor = hwMap.gyroSensor.get("gyroSensor");
 
         //Sets spin directions to make writing power easier
-        arm.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set all motors to zero power to prevent it from accidentally turning on
         arm.setPower(0);
